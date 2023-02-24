ca877fe51bf242087dbdd5ed742237f27cbfeaa7
DenHelloWorld <denissaveliev23@gmail.com>
2023-02-24 14:16:20 +0200

    feat: fix

--- src/index.js
+++ src/index.js
@@ -1,16 +1,53 @@
-module.exports = toReadable = (number) => {
-        const ones = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-        const tens = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
-        const teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-        const hundreds = ['one hundred', 'two hundred', 'three hundred', 'four hundred', 'five hundred', 'six hundred', 'seven hundred', 'eight hundred', 'nine hundred'];
-      
-        let onesVal = number % 10;
-        let tensVal = (number % 100 - onesVal) / 10;
-        let hundredsVal = (number - (tensVal * 10) - onesVal) / 100;
-      
-        if (tensVal === 1) {
-          return `${hundreds[hundredsVal]} ${teens[onesVal]}`;
-        } else {
-          return `${hundreds[hundredsVal]} ${tens[tensVal]} ${ones[onesVal]}`;
-        }
-      };
+module.exports = function toReadable (number) {
+  const ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  const tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+  const thousands = ['', 'thousand', 'million', 'billion'];
+
+  let numString = number.toString();
+  let numLength = numString.length;
+  let i = 0;
+  let result = '';
+  if (number === 0) {
+    return "zero"
+  } 
+  while (numLength > 0) {
+    let chunkLength = numLength - 3;
+    if (chunkLength < 0) {
+      chunkLength = 0;
+    }
+
+    let chunk = numString.substr(chunkLength, numLength);
+    let chunkNum = parseInt(chunk);
+
+    let h = Math.floor(chunkNum / 100);
+    let t = Math.floor((chunkNum % 100) / 10);
+    let o = Math.floor(chunkNum % 10);
+
+    let chunkString = '';
+
+    if (h > 0) {
+      chunkString += ones[h] + ' hundred';
+    }
+
+    if (t > 1) {
+      chunkString += (h > 0 ? ' ' : '') + tens[t];
+      if (o > 0) {
+        chunkString += ' ' + ones[o];
+      }
+    } else if (t === 1) {
+      chunkString += (h > 0 ? ' ' : '') + ones[t * 10 + o];
+    } else {
+      chunkString += (h > 0 ? ' ' : '') + ones[o];
+    }
+
+    if (chunkNum > 0) {
+      chunkString += (chunkString.length > 0 ? ' ' : '') + thousands[i];
+    }
+
+    result = chunkString + (result.length > 0 ? ' ' : '') + result;
+
+    numLength -= 3;
+    i++;
+  }
+return result.trimEnd()
+}
